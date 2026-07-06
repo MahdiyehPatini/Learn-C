1352c4593faea20dcee3d846c2f9506159e6c70b
Mahdiyeh Patini <mhpt1370@gmail.com>
2026-07-06 15:07:10 +0330

    jalase4

---
+++ jalase4/jalase4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define number1 60
+int main(){
+    /* روش اول تعریف متغییر */
+
+	//int a=10;
+	//char b="a";
+	//float c=13.5;
+	//double d=145.5;
+
+    // روش دوم تعریف متغییر
+	//int age;
+	//age=37;
+
+	//char Name;
+	//Name="a";
+
+	//float average;
+	//avarage=13.5;
+
+	//double scoure;
+	//scoure=14.5;
+
+    //int number1;
+    //printf("enter your number1:");
+    //scanf("%d",&number1);
+    //printf("your number 1 is:%d",number1);
+
+
+    //int number1,number2;
+    //printf("enter number1, number2:");
+    //scanf("%d%d",&number1,&number2);
+    //printf("number1 is %d number2 is %d",number1,number2);
+
+
+    //روش سوم متغییر ثابت
+    //const int number2 =40;
+    //printf("number2 is:%d",number2);
+
+
+}
---
+++ prj/calculator/calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+	int number1,number2,result;
+
+	printf("enter number1:");
+
+	scanf("%d",&number1);
+
+	printf("enter number2:");
+
+	scanf("%d",&number2);
+
+    result = number1 + number2;
+
+	printf("your result is: %d + %d = %d ",number1,number2,result);
+
+}
